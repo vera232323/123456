2fb5b41dc73a661278249954a779b0fdc92ea91d
Vera <imbesom@mail.ru>
2022-09-29 01:27:21 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -20,6 +20,30 @@ if(palifive > 9999 && palifive <= 99999)
    }
 else
 {Console.WriteLine("Число не подходит под параметры ввода!!!!");}
-
 }
-Palindrom();
+
+// Palindrom();
+
+
+
+void treede ()
+{
+Console.WriteLine("Введите координату точки А по оси X");
+    int ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по оси Y");
+    int ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по оси Z");
+    int az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки Б по оси X");
+    int bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки Б по оси Y");
+    int by = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки Б по оси Z");
+    int bz = Convert.ToInt32(Console.ReadLine());
+int x = ax-bx;
+int y = ay-by;
+int z = az-bz;
+double length = Math.Sqrt(x * x + y * y + z * z);
+Console.WriteLine($"Длина отрезка {length}");}
+treede ();
+
